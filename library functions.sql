cce25994d27ab7aba79f5e92c4a29ca21ee185d0
lucasakerlund <46222535+lucasakerlund@users.noreply.github.com>
2022-05-23 11:15:54 +0200

    added some stored procedures

--- library functions.sql
+++ library functions.sql
@@ -2,6 +2,9 @@ DROP PROCEDURE IF EXISTS `loan_book`;
 DELIMITER //
 CREATE PROCEDURE `loan_book` (library_id INT, ISBN VARCHAR(255), customer_id INT, loan_date VARCHAR(10), return_date VARCHAR(10), OUT succeed INT)
 BEGIN
+	DECLARE `rollback` BOOL DEFAULT 0;
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET `rollback` = 1;
+    
 	START TRANSACTION;
 		SET @bookId = "";
 		SELECT
@@ -17,6 +20,10 @@ BEGIN
 		INTO @bookId;
 		INSERT INTO loans VALUES(@bookId, customer_id, loan_date, return_date);
         SET succeed = 1;
+        IF `rollback` THEN
+			ROLLBACK;
+            SET succeed = 0;
+		END IF;
     COMMIT;
 END//
 
@@ -29,19 +36,38 @@ CREATE PROCEDURE `get_books`()
 BEGIN
 	SELECT
     bd.*,
-    (SELECT GROUP_CONCAT(`name`) from `books_with_authors` ba, `authors` a WHERE bd.`isbn` = ba.`isbn` AND ba.`author_id` = a.`author_id`) AS authors,
-    (SELECT GROUP_CONCAT(`name`) from `books_with_genre` bg, `genre` g WHERE bd.`isbn` = bg.`isbn` AND bg.`genre_id` = g.`genre_id`) AS genres
+    (SELECT GROUP_CONCAT(`name`) FROM `books_with_authors` ba, `authors` a WHERE bd.`isbn` = ba.`isbn` AND ba.`author_id` = a.`author_id`) AS authors,
+    (SELECT GROUP_CONCAT(`name`) FROM `books_with_genre` bg, `genre` g WHERE bd.`isbn` = bg.`isbn` AND bg.`genre_id` = g.`genre_id`) AS genres,
+    (SELECT GROUP_CONCAT(DISTINCT l.`name`) FROM `libraries` l, `books` b WHERE bd.`isbn` = b.`isbn` AND b.`library_id` = l.`library_id` AND NOT EXISTS(SELECT * FROM `loans` l WHERE b.`book_id` = l.`book_id`)) AS available_libraries
     FROM `book_details` bd
     GROUP BY bd.`isbn`;
 END //
+
+DROP PROCEDURE IF EXISTS `get_amount_of_books_in_stock`;
+DELIMITER //
+CREATE PROCEDURE `get_amount_of_books_in_stock`(isbn VARCHAR(50))
+BEGIN
+	SELECT
+    COUNT(*) AS amount
+    FROM `books` b
+    WHERE
+    b.`isbn` = isbn AND
+    NOT EXISTS(
+		SELECT *
+		FROM `loans` l
+		WHERE b.`book_id` = l.`book_id`
+    );
+END //
+
+CALL `get_amount_of_books_in_stock`("9781387207770");
 
 DROP PROCEDURE IF EXISTS `get_book_by_id`
 DELIMITER //
 CREATE PROCEDURE `get_book_by_id`(bookId INT)
 BEGIN
 	SELECT b.`book_id`, b.`isbn`, b.`library_id`, bd.`title`, bd.`description`, bd.`language`, bd.`published`, bd.`image_source`, bd.`pages`,
-    (SELECT GROUP_CONCAT(`name`) from `books_with_authors` ba, `authors` a WHERE bd.`isbn` = ba.`isbn` AND ba.`author_id` = a.`author_id`) AS authors,
-    (SELECT GROUP_CONCAT(`name`) from `books_with_genre` bg, `genre` g WHERE bd.`isbn` = bg.`isbn` AND bg.`genre_id` = g.`genre_id`) AS genres
+    (SELECT GROUP_CONCAT(`name`) FROM `books_with_authors` ba, `authors` a WHERE bd.`isbn` = ba.`isbn` AND ba.`author_id` = a.`author_id`) AS authors,
+    (SELECT GROUP_CONCAT(`name`) FROM `books_with_genre` bg, `genre` g WHERE bd.`isbn` = bg.`isbn` AND bg.`genre_id` = g.`genre_id`) AS genres
 	FROM `books` b, `book_details` bd
 	WHERE b.`book_id` = bookId AND b.`isbn` = bd.`isbn`;
 END //
@@ -64,6 +90,24 @@ BEGIN
 END //
 
 CALL available_amount_of_book_in_libraries("9781387207770");
+
+DROP PROCEDURE IF EXISTS `get_loaned_books_with_isbn`;
+DELIMITER //
+CREATE PROCEDURE `get_loaned_books_with_isbn`(isbn VARCHAR(50))
+BEGIN
+	SELECT
+    *
+    FROM `loans` l
+    WHERE
+    EXISTS(
+		SELECT *
+        FROM `books` b
+        WHERE b.`isbn` = isbn AND
+        l.`book_id` = b.`book_id`
+    );
+END //
+
+CALL get_loaned_books_with_isbn("1251253223423");
 
 DROP PROCEDURE IF EXISTS `create_customer`;
 DELIMITER //
